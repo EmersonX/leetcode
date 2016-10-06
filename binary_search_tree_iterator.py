7dbd538fe3f413a793aff389da2b642a9707aa3c
王梓键 <wangzj@mama.cn>
2016-10-06 19:08:27 +0800

    update

--- binary_search_tree_iterator.py
+++ binary_search_tree_iterator.py
@@ -1,12 +1,26 @@
+# Time:  O(1)
+# Space: O(h), h is height of binary tree
+# 
+# Implement an iterator over a binary search tree (BST).
+# Your iterator will be initialized with the root node of a BST.
+# 
+# Calling next() will return the next smallest number in the BST.
+# 
+# Note: next() and hasNext() should run in average O(1) time
+# and uses O(h) memory, where h is the height of the tree.
+#
+
 # Definition for a  binary tree node
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class EmptyStackError(Exception):
     pass
+
 
 class Stack(object):
     def __init__(self):
@@ -32,6 +46,9 @@ class Stack(object):
 
 
 class BSTIterator(object):
+    """
+    利用stack记录遍历时的节点，典型的用空间换时间的算法。
+    """
     def __init__(self, root):
         """
         :type root: TreeNode
